452d0a22e30e77534173294cceb66bc98a756e7a
Dang Van <dinvvan@gmail.com>
2018-10-21 21:45:57 +0700

    basic-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   validates :email, length: {maximum: 255},uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
   has_secure_password
   validates :password, length: {minimum: 6}
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create string, cost: cost
+  end
 end
