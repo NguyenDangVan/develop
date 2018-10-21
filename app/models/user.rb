8b57ed4cf6e275e502d43e982a6e956618ef239e
LeTuan252 <lethanhtuan252@gmail.com>
2018-10-21 23:26:41 +0700

    checkbox remember

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save {email.downcase!}
   has_many :favorite
   has_many :playlist
@@ -8,11 +9,27 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, length: {maximum: 255},uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
   has_secure_password
-  validates :password, length: {minimum: 6}
+  validates :password, length: {minimum: 8}
 
   def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create string, cost: cost
   end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_041423) do
+ActiveRecord::Schema.define(version: 2018_10_21_154041) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2018_10_21_041423) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
