cecaa2971df93083ca60d88aaa57a41a9da47ddf
Dang Van <dinvvan@gmail.com>
2018-10-21 10:03:59 +0700

    sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  before_save {email.downcase!}
   has_many :favorite
   has_many :playlist
-  validates :name, :email, :age, presence: true
-  validates :name, length: {maximum: Settings.models.maximum_name}
-  validates :age, numericality: true, length: {maximum: Settings.models.maximum_age}
+  validates :name, :email, :age, :password, presence: true
+  validates :name, length: {maximum: 30}
+  validates :age, numericality: true, length: {maximum: 3}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validate :email,length: {maximum: Settings.models.maximum_email} uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+  validates :email, length: {maximum: 255},uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
+  validates :password, length: {minimum: 6}
 end
