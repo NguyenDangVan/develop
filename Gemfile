683a6dd9794ca81b6dbbe4df2b73319cdc248658
LeTuan252 <lethanhtuan252@gmail.com>
2018-10-22 12:57:20 +0700

    activation

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem "bootsnap", ">= 1.1.0", require: false
 gem "jquery-rails"
 
 group :development, :test do
+  gem "sqlite3"
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_154041) do
+ActiveRecord::Schema.define(version: 2018_10_22_024020) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -82,6 +82,9 @@ ActiveRecord::Schema.define(version: 2018_10_21_154041) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
