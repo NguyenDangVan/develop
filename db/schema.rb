0f08866922d4296761a73ea192aa5a838a6eecc8
Dang Van <dinvvan@gmail.com>
2018-10-24 00:59:20 +0700

    add db

--- db/schema.rb
+++ db/schema.rb
@@ -82,8 +82,8 @@ ActiveRecord::Schema.define(version: 2018_10_22_024853) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.boolean "admin", default: false
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
