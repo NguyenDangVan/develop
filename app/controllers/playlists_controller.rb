f7b25eb69afe80147278cbe2bb8de876ece5d0dd
LeTuan252 <lethanhtuan252@gmail.com>
2018-11-14 15:31:45 +0700

    edit playlist description, reply

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -4,7 +4,8 @@ class PlaylistsController < ApplicationController
   def show
     @user = User.find_by id: params[:user_id]
     @playlist = @user.playlists.find_by id: params[:id]
-    @comments = @playlist.comments.where(parent_id: nil)
+    @comment5 = @playlist.comments.where(parent_id: nil).first(5)
+    @comment6_to_last = @playlist.comments[5..-1]
     @songs = Song.all.rank_song
     if @user.playlists.exists?(id: params[:id])
       @comment = Comment.new
